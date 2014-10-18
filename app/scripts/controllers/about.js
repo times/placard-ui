0329953928998dd1660e75968b708678ae9134fa
Chris Hutchinson <chris_hutchinson@me.com>
2014-10-18 11:28:18 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name placardUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the placardUiApp
+ */
+angular.module('placardUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name placardUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the placardUiApp
+ */
+angular.module('placardUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
